53dacf8e499a9e781c9a514bb357455e54d80848
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2023-01-05 07:42:31 -0800

    feature: add toggle button that toggle book status on my book instance object

--- script.js
+++ script.js
@@ -1,44 +1,65 @@
 const books = [];
 
 function Book(title, author, pages, isRead) {
-   this.title = title,
-   this.author = author,
-   this.pages = pages,
-   this.isRead = isRead
-}
-
-Book.prototype.info = function() {
-   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "Read" : "Not Read"}`;
+	(this.title = title), (this.author = author), (this.pages = pages), (this.isRead = isRead);
 }
 
 function addBook(title, author, pages, isRead) {
-   const book = new Book(title, author, pages, isRead)
-   books.push(book);
+	const book = new Book(title, author, pages, isRead);
+	books.push(book);
 }
 
 addBook("Prisoner of Azkaban", "JK Rowling", 222, false);
 addBook("Sorcerer's Stone", "JK Rowling", 234, true);
+addBook("Prisoner of Azkaban", "JK Rowling", 222, false);
+addBook("Sorcerer's Stone", "JK Rowling", 234, true);
+addBook("Prisoner of Azkaban", "JK Rowling", 222, false);
+addBook("Sorcerer's Stone", "JK Rowling", 234, true);
+addBook("Prisoner of Azkaban", "JK Rowling", 222, false);
+addBook("Sorcerer's Stone", "JK Rowling", 234, true);
 
 function displayBook() {
-   books.forEach(book => {
-      const bookContainer = document.createElement("div");
-      const title = document.createElement("p");
-      const author = document.createElement("p")
-      const pages = document.createElement("p");
-      const isRead = document.createElement("p");
+	books.forEach((book, i) => {
+		const bookContainer = document.createElement("div");
+		const title = document.createElement("p");
+		const author = document.createElement("p");
+		const pages = document.createElement("p");
+		const isRead = document.createElement("p");
+		const removeButton = document.createElement("button");
+		const toggleButton = document.createElement("button");
 
-      title.innerText = book.title;
-      author.innerText = book.author;
-      pages.innerText = book.pages;
-      isRead.innerText = book.isRead ? "Read" : "Not Read";
+		title.innerText = book.title;
+		author.innerText = book.author;
+		pages.innerText = book.pages;
+		isRead.innerText = book.isRead ? "Read" : "Not Read";
+		removeButton.innerText = "Remove";
+		toggleButton.innerText = "Toggle Status";
 
-      bookContainer.className = "book-container";
+		bookContainer.className = "book-container";
+		bookContainer.dataset.id = i;
 
-      bookContainer.append(title, author, pages, isRead);
-      document.body.appendChild(bookContainer);
-   });
+		bookContainer.append(title, author, pages, isRead, removeButton, toggleButton);
+		document.body.appendChild(bookContainer);
+
+		removeButton.addEventListener("click", e => {
+			books.splice(e.target.parentElement.dataset.id, 1);
+			while (document.body.firstChild) {
+				document.body.removeChild(document.body.firstChild);
+			}
+			displayBook();
+		});
+
+		toggleButton.addEventListener("click", e => {
+			e.target.parentElement.dataset.id;
+			books[i].isRead = !books[i].isRead;
+         isRead.innerText = book.isRead ? "Read" : "Not Read";
+			console.log(books[i]);
+         console.log(e.target.parentElement.dataset.id);
+         console.log(books);
+		});
+	});
 }
 
 displayBook();
 
-
+console.log(books);
